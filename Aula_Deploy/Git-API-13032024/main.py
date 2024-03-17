3eae3a491123933da1533c77d242534ca1963eae
paulofjm <98480159+paulofjm@users.noreply.github.com>
2024-03-17 16:43:43 -0300

    initial change

--- Aula_Deploy/Git-API-13032024/main.py
+++ Aula_Deploy/Git-API-13032024/main.py
@@ -4,7 +4,7 @@ from art import tprint
 
 
 def main(args):
-    tprint('Oficina - Data Mining')
+    tprint('Oficina - Data Mining - Deploy')
     os.system("uvicorn app:app --reload --port " + args.port)
 
 
@@ -12,3 +12,5 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='API Oficina')
     parser.add_argument('--port', type=str, dest='port', help='Port ', default='5000')
     main(parser.parse_args())
+
+
