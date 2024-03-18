65b64e841844df858c14ce928908c4b6676680a2
paulofjm <98480159+paulofjm@users.noreply.github.com>
2024-03-18 14:41:30 -0300

    refactor: alter code of main.py for include directory

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import os
 import argparse
 from art import tprint
 
+"""
 if __name__ == '__main__':
     os.system('uvicorn app:app --reload')
-
 """
+
 def main(args):
     tprint('Oficina - Data Mining - Deploy')
 
@@ -13,7 +14,7 @@ def main(args):
     current_directory = os.path.dirname(os.path.abspath(__file__))
 
     # Diretório da subpasta que contém app.py
-    app_directory = os.path.join(current_directory, "Git-API-13032024")
+    app_directory = os.path.join(current_directory, "Aula_Deploy", "Git-API-13032024")
 
     # Comando para iniciar o Uvicorn com o módulo app
     os.system(f'uvicorn app:app --reload --app-dir "{app_directory}"' )
@@ -24,4 +25,3 @@ if __name__ == '__main__':
     parser.add_argument('--port', type=str, dest='port', help='Port ', default='5000')
     main(parser.parse_args())
 
-"""
